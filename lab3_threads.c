de71097d4d53e013c6b7ba57c307dd4776f2b35d
Victor Sales <salesrvictor@gmail.com>
2019-04-13 16:18:14 -0300

    Added threaded output

--- lab3_threads.c
+++ lab3_threads.c
@@ -4,16 +4,17 @@
  ** de N
  */
 
-#define MAX_THREADS 100
-#define MAX_ARRAY 100
+#define MAX_THREADS 4
+#define MAX_ARRAY 1000000
 #define true  1
 #define false 0
 #define WINAPI
 #define debugtxt(FORMAT) printf(" TID %d: " #FORMAT "\n",(int) pthread_self())
 #define debug(FORMAT, ARGS...) printf("TID %d: " #FORMAT "\n",(int) pthread_self(),ARGS)
 
-#include <pthread.h>
+#include <time.h>
 #include <stdio.h>
+#include <pthread.h>
 #include <semaphore.h>
 
 // Funcoes e variaveis do problema
@@ -48,7 +49,7 @@ void* WINAPI threadFunc(void * lpparam){
 		// Condição de corrida das threads no iterador do array
 		down(&iterMutex);
 		i = iter++;
-		printf("i = %d\n", i);
+		// printf("i = %d\n", i);
 		up(&iterMutex);
 
 		// Precisamos percorrer o array apenas até sua metade
@@ -81,6 +82,7 @@ int main() {
 
 	fclose(entry);
 
+        clock_t start = clock();
 	for (int i=0; i<p; i++){
 		pthread_create (&threadId[i],
 				NULL,
@@ -91,11 +93,13 @@ int main() {
 	for(int i=0; i<p; i++) {
 		pthread_join (threadId[i], NULL);		
 	}
+        clock_t end = clock();
 
 	printf("\nArray trocado:\n");
 	for (int i=0; i<n; i++)
 		printf("%d\n", array[i]);
 	printf("\n");
 
+        printf("Tempo: %g ms\n", 1000*(double)(end-start)/CLOCKS_PER_SEC);
 	return 0;
 }
