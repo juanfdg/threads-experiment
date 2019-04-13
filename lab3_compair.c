b0bc7d819441a3c8991c05f87dbee5919518c942
name <juanfdantas@gmail.com>
2019-04-13 16:01:00 -0300

    Compair seq and threads

--- lab3_compair.c
+++ lab3_compair.c
@@ -1,25 +1,21 @@
-/** Programa para retornar se um número N é perfeito ou não,
- ** iterando pelos números de 1 a N de com processamento
- ** dividido entre P threads, detectando quais são os fatores
- ** de N
- */
-
 #define MAX_THREADS 100
-#define MAX_ARRAY 100
+#define MAX_ARRAY 1000000
+#define WINAPI
 #define true  1
 #define false 0
-#define WINAPI
 #define debugtxt(FORMAT) printf(" TID %d: " #FORMAT "\n",(int) pthread_self())
 #define debug(FORMAT, ARGS...) printf("TID %d: " #FORMAT "\n",(int) pthread_self(),ARGS)
 
 #include <pthread.h>
 #include <stdio.h>
 #include <semaphore.h>
+#include <time.h>
 
 // Funcoes e variaveis do problema
 int n, p;
 int iter;
-int array[MAX_ARRAY];
+int arrayThreads[MAX_ARRAY];
+int arraySequential[MAX_ARRAY];
 
 // Funcoes e variaveis das threads
 pthread_cond_t handleThread[MAX_THREADS];
@@ -28,8 +24,8 @@ sem_t iterMutex;
 sem_t factorsMutex;
 
 // Truque para sabermos qual o semaforo foi chamado e poder imprimi-lo
-#define up(SEM) _up(SEM,#SEM)
-#define down(SEM) _down(SEM,#SEM)
+#define up(SEM) sem_post(SEM)
+#define down(SEM) sem_wait(SEM)
 
 void _up(sem_t *sem, const char * name) {
 	debug("Up %s ...",name);
@@ -42,21 +38,20 @@ void _down(sem_t *sem, const char * name) {
 	debug("Down %s complete!",name);
 }
 
-unsigned long WINAPI threadFunc(void * lpparam){
+unsigned long WINAPI threadFunc(void * lpparam) {
 	int i, aux;
 	while (true) {
 		// Condição de corrida das threads no iterador do array
 		down(&iterMutex);
 		i = iter++;
-		printf("i = %d\n", i);
+		//printf("i = %d\n", i);
 		up(&iterMutex);
 
 		// Precisamos percorrer o array apenas até sua metade
 		if (i > n/2) break;
 
-		aux = array[i];
-		array[i] = array[n-i-1]; 
-		array[n-i-1] = aux;
+		arrayThreads[i] ^= arrayThreads[n-i-1] ^= 
+		arrayThreads[i] ^= arrayThreads[n-i-1];
 	}
 }
 
@@ -68,20 +63,24 @@ int main() {
 	sem_init(&factorsMutex, 0, 1);
 
 	FILE* entry = fopen("entry.txt", "r");
+	FILE* outThreads = fopen("out_threads.txt", "w"); 
+	FILE* outSequential = fopen("out_sequential.txt", "w");
 
 	printf("Digite N e o número de threads:\n");
 	scanf("%d %d", &n, &p);
 
-	printf("Array inicial:\n");
 	for (int i=0; i<n; i++) {
-		fscanf(entry, "%d ", &array[i]);
-		printf("%d\n", array[i]);
+		fscanf(entry, "%d ", &arrayThreads[i]);
+		arraySequential[i] = arrayThreads[i];
 	}
-	printf("\n");
-
 	fclose(entry);
 
-	for (int i=0; i<p; i++){
+	clock_t start, end;
+    double cpuTimeUsedThreads;
+	double cpuTimeUsedSequential;
+
+	start = clock();
+	for (int i=0; i<p; i++) {
 		pthread_create (&threadId[i],
 				NULL,
 				threadFunc,
@@ -91,11 +90,28 @@ int main() {
 	for(int i=0; i<p; i++) {
 		pthread_join (threadId[i], NULL);		
 	}
+	end = clock();
+	cpuTimeUsedThreads = ((double) (end - start)) / CLOCKS_PER_SEC;
 
-	printf("\nArray trocado:\n");
-	for (int i=0; i<n; i++)
-		printf("%d\n", array[i]);
+	for (int i=0; i<n; i++) {
+		fprintf(outThreads, "%d\n", arrayThreads[i]);
+	}
+
+	start = clock();
+	int aux;
+	for (int i=0; i < n/2+1; i++) {
+		 arraySequential[i] ^= arraySequential[n-i-1] ^= 
+		 arraySequential[i] ^= arraySequential[n-i-1];
+	}
+	end = clock();
+	cpuTimeUsedSequential = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+	for (int i=0; i<n; i++) {
+		fprintf(outSequential, "%d\n", arraySequential[i]);
+	}
+
 	printf("\n");
-
+	printf("Tempo gasto com threads: %f\n\n", cpuTimeUsedThreads);
+	printf("Tempo gasto sequencial: %f\n\n", cpuTimeUsedSequential);
 	return 0;
 }
